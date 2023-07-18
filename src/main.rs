5298b4927e894b1df55d1468467545f2cbe46512
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-07-18 19:51:13 +0300

    Call factory contract

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
-use ethers::prelude::{Provider, Http, U64, Middleware};
+use ethers::prelude::Middleware;
 use std::fs;
 use eyre::Result;
+mod abi;
 
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let api_key = fs::read_to_string("APIKEY")?;
-    let url = format!("https://mainnet.infura.io/v3/{}", api_key);
-    let provider = Provider::<Http>::try_from(url)?;
+    println!("Creating client...");
+    let client = abi::get_client(&api_key)?;
+    println!("Client created.");
 
-    let block_number: U64 = provider.get_block_number().await?;
-    println!("Block no. {}", block_number);
+    let block_number = client.clone().get_block_number().await?;
+    println!("Block no. {block_number}");
 
-    let block = provider.get_block(block_number).await?;
-    let block_text = serde_json::to_string(&block)?;
-    println!("Got block: {}", block_text);
+    // let block = client.clone().get_block(block_number).await?;
+    // let block_data = serde_json::to_string(&block)?;
+    // println!("Block data: {block_data}");
+
+    abi::test_contract_call(client.clone()).await?;
+
+    let result = abi::get_pools(client.clone()).await?;
+    println!("Pools: {result:?}");
+
+    // let (r0, r1) = abi::get_trade_info(client.clone(), ADDRESS).await?;
+    // println!("Trade info: {r0:?}, {r1:?}");
 
     Ok(())
 }
