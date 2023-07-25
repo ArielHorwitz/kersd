3002a4dc6c94964e0583e7a303d93092633b96c0
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-07-25 13:53:51 +0300

    Proper logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+
 use ethers::prelude::{Address, U512};
-use eyre::{eyre, Error, Result};
+use eyre::{eyre, Result};
 use serde::{Deserialize, Serialize};
 use std::{env, path::Path, time::Duration};
 use tokio::time::{sleep, timeout};
@@ -10,7 +14,8 @@ const DB_PATH: &str = "./db";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("KERSD DAEMON STARTING");
+    pretty_env_logger::init();
+    info!("Starting kersd daemon");
     // Setup
     let api_key = env::var("APIKEY")
         .expect("missing APIKEY environment variable")
@@ -18,11 +23,13 @@ async fn main() -> Result<()> {
     let client = api::get_client(&api_key)?;
     let db_dir = env::var("DB_PATH").unwrap_or(DB_PATH.to_owned());
     let db_dir = Path::new(&db_dir);
+    info!("Data directory: {db_dir:?}");
     tokio::fs::create_dir_all(db_dir).await?;
     let poll_interval: u64 = env::var("POLL_INTERVAL_MS")
         .unwrap_or(POLL_INTERVAL_MS.to_string())
         .parse()
         .expect("Invalid POLL_INTERVAL_MS");
+    info!("Poll interval: {poll_interval} ms");
     let poll_interval = Duration::from_millis(poll_interval);
     let poll_timeout = Duration::from_millis(1);
     let mut last_block_number = api::get_block_number(&client.clone())
@@ -30,16 +37,15 @@ async fn main() -> Result<()> {
         .expect("failed to get block number, maybe invalid APIKEY?");
     let mut task_handlers = tokio::task::JoinSet::new();
     let all_pools = api::get_all_pools(&client).await?;
-    println!("Found {} pools", all_pools.len());
+    info!("Found {} pools", all_pools.len());
     // Start daemon loop
+    info!("Starting daemon loop");
     loop {
         // Spawn tasks if new block exists
+        debug!("Polling for new blocks >={}", last_block_number + 1);
         let block_number = api::get_block_number(&client).await?;
         if block_number > last_block_number {
-            println!(
-                "Block number: {block_number} (spawning {} tasks)",
-                all_pools.len()
-            );
+            info!("New block: {block_number}");
             last_block_number = block_number;
             let block_dir = db_dir.join(format!("{block_number}"));
             tokio::fs::create_dir_all(&block_dir).await?;
@@ -55,24 +61,26 @@ async fn main() -> Result<()> {
             // Wrap join_next in a timeout so that it behaves more like polling
             match timeout(poll_timeout, task_handlers.join_next()).await {
                 // Timed out (no tasks done, stop polling)
-                Err(_) => break,
+                Err(_) => {
+                    trace!("Awaiting {} tasks", task_handlers.len());
+                    break;
+                }
                 // JoinSet is empty (no tasks pending, stop polling)
-                Ok(None) => break,
+                Ok(None) => {
+                    trace!("All {} tasks done", all_pools.len());
+                    break;
+                }
                 // Joining task failed (log and poll again)
-                Ok(Some(Err(err))) => log(eyre!(err)),
+                Ok(Some(Err(err))) => error!("{err}"),
                 // A task has failed (log and poll again)
-                Ok(Some(Ok(Err(err)))) => log(eyre!(err)),
+                Ok(Some(Ok(Err(err)))) => error!("{err}"),
                 // A task has completed successfully (poll again)
-                Ok(Some(Ok(Ok(_)))) => (),
+                Ok(Some(Ok(Ok(completed)))) => trace!("Completed task: {completed:?}"),
             }
         }
         // Loop interval
         sleep(poll_interval).await;
     }
-}
-
-fn log(err: Error) {
-    eprintln!("{}", format!("{err:?}").replace('\n', " "));
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -90,7 +98,7 @@ async fn collect_exchange_rate(
     pool: Address,
     block_number: u64,
     db_dir: String,
-) -> Result<()> {
+) -> Result<ExchangeRate> {
     // Wrap inner function to catch errors and include context
     do_collect_exchange_rate(client, pool, block_number, db_dir)
         .await
@@ -102,7 +110,7 @@ async fn do_collect_exchange_rate(
     pool: Address,
     block_number: u64,
     db_dir: String,
-) -> Result<()> {
+) -> Result<ExchangeRate> {
     let ti = api::get_trade_info(&client, &pool).await?;
     let buy_amount = U512::exp10(10);
     let sell0 = api::calc_exchange_rate(ti.clone(), buy_amount)?;
@@ -117,5 +125,5 @@ async fn do_collect_exchange_rate(
     let json_string = serde_json::to_string_pretty(&exchange_rate)?;
     let filepath = Path::new(&db_dir).join(format!("{pool:?}"));
     tokio::fs::write(filepath, json_string.into_bytes()).await?;
-    Ok(())
+    Ok(exchange_rate)
 }
